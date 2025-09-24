0c23e77ebdf459265c8157f4cf565c67595ecc68
Cian Leypoldt <cianleypoldt@gmail.com>
2025-09-25 01:08:14 +0200

    1000 build system fails and still doesn't build

--- src/velm_data-representation/ndarray.h
+++ src/velm_data-representation/ndarray.h
@@ -10,13 +10,13 @@ namespace vlem_DR {
  *
  */
 
-template <typename T, std::size_t N> struct grid {
+template <typename T, std::size_t N> struct ndarray {
     T *         data = nullptr;
     std::size_t dims[N];
     std::size_t strides[N];
 
-    template <typename... Idx> grid(Idx... idx);
-    ~grid();
+    template <typename... Idx> ndarray(Idx... idx);
+    ~ndarray();
 
     template <typename... Idx> [[nodiscard]] T &       at(Idx... idx);
     template <typename... Idx> [[nodiscard]] const T & at(Idx... idx) const;
@@ -32,13 +32,13 @@ template <typename T, std::size_t N> struct grid {
 
     template <typename... Idx> [[nodiscard]] T &       operator()(Idx... idx);
     template <typename... Idx> [[nodiscard]] const T & operator()(Idx... idx) const;
-    [[nodiscard]] grid &                               operator=(const grid & B);
-    [[nodiscard]] grid &                               operator=(grid && B) noexcept;
-    grid(const grid & B);
-    grid(grid && B) noexcept;
+    [[nodiscard]] ndarray &                            operator=(const ndarray & B);
+    [[nodiscard]] ndarray &                            operator=(ndarray && B) noexcept;
+    ndarray(const ndarray & B);
+    ndarray(ndarray && B) noexcept;
 };
 
-template <typename T, std::size_t N> template <typename... Idx> grid<T, N>::grid(Idx... idx) {
+template <typename T, std::size_t N> template <typename... Idx> ndarray<T, N>::ndarray(Idx... idx) {
     static_assert(sizeof...(Idx) == N, "Number of indices must match grid dimension");
     std::size_t indices[N] = { static_cast<std::size_t>(idx)... };
     for (std::size_t i = 0; i < N; ++i) {
@@ -54,11 +54,11 @@ template <typename T, std::size_t N> template <typename... Idx> grid<T, N>::grid
     data = new T[total_elements()]{};
 }
 
-template <typename T, std::size_t N> grid<T, N>::~grid() {
+template <typename T, std::size_t N> ndarray<T, N>::~ndarray() {
     delete[] data;
 }
 
-template <typename T, std::size_t N> template <typename... Idx> [[nodiscard]] T & grid<T, N>::at(Idx... idx) {
+template <typename T, std::size_t N> template <typename... Idx> [[nodiscard]] T & ndarray<T, N>::at(Idx... idx) {
     std::size_t indices[N] = { static_cast<std::size_t>(idx)... };
     for (std::size_t i = 0; i < N; ++i) {
         if (indices[i] >= dims[i]) {
@@ -68,7 +68,7 @@ template <typename T, std::size_t N> template <typename... Idx> [[nodiscard]] T 
     return data[offset_of_index(idx...)];
 }
 
-template <typename T, std::size_t N> template <typename... Idx> const T & grid<T, N>::at(Idx... idx) const {
+template <typename T, std::size_t N> template <typename... Idx> const T & ndarray<T, N>::at(Idx... idx) const {
     std::size_t indices[N] = { static_cast<std::size_t>(idx)... };
     for (std::size_t i = 0; i < N; ++i) {
         if (indices[i] >= dims[i]) {
@@ -79,7 +79,7 @@ template <typename T, std::size_t N> template <typename... Idx> const T & grid<T
 }
 
 template <typename T, std::size_t N> template <typename... Idx>
-std::size_t grid<T, N>::offset_of_index(const Idx &... idx) const {
+std::size_t ndarray<T, N>::offset_of_index(const Idx &... idx) const {
     static_assert(sizeof...(Idx) == N, "Number of indices must match grid dimension");
     std::size_t indices[N] = { static_cast<std::size_t>(idx)... };
     std::size_t offset     = 0;
@@ -89,7 +89,7 @@ std::size_t grid<T, N>::offset_of_index(const Idx &... idx) const {
     return offset;
 }
 
-template <typename T, std::size_t N> std::size_t grid<T, N>::total_elements() const {
+template <typename T, std::size_t N> std::size_t ndarray<T, N>::total_elements() const {
     std::size_t element_count = 1;
     for (std::size_t i = 0; i < N; ++i) {
         element_count *= dims[i];
@@ -97,37 +97,37 @@ template <typename T, std::size_t N> std::size_t grid<T, N>::total_elements() co
     return element_count;
 }
 
-template <typename T, std::size_t N> T * grid<T, N>::begin() {
+template <typename T, std::size_t N> T * ndarray<T, N>::begin() {
     return data;
 }
 
-template <typename T, std::size_t N> const T * grid<T, N>::begin() const {
+template <typename T, std::size_t N> const T * ndarray<T, N>::begin() const {
     return data;
 }
 
-template <typename T, std::size_t N> T * grid<T, N>::end() {
+template <typename T, std::size_t N> T * ndarray<T, N>::end() {
     return data + total_elements();
 }
 
-template <typename T, std::size_t N> const T * grid<T, N>::end() const {
+template <typename T, std::size_t N> const T * ndarray<T, N>::end() const {
     return data + total_elements();
 }
 
-template <typename T, std::size_t N> void grid<T, N>::fill(T value) {
+template <typename T, std::size_t N> void ndarray<T, N>::fill(T value) {
     for (std::size_t i = 0; i < total_elements(); ++i) {
         data[i] = value;
     }
 }
 
-template <typename T, std::size_t N> template <typename... Idx> T & grid<T, N>::operator()(Idx... idx) {
+template <typename T, std::size_t N> template <typename... Idx> T & ndarray<T, N>::operator()(Idx... idx) {
     return data[offset_of_index(idx...)];
 }
 
-template <typename T, std::size_t N> template <typename... Idx> const T & grid<T, N>::operator()(Idx... idx) const {
+template <typename T, std::size_t N> template <typename... Idx> const T & ndarray<T, N>::operator()(Idx... idx) const {
     return data[offset_of_index(idx...)];
 }
 
-template <typename T, std::size_t N> grid<T, N> & grid<T, N>::operator=(const grid & B) {
+template <typename T, std::size_t N> ndarray<T, N> & ndarray<T, N>::operator=(const ndarray & B) {
     // self-assignment check
     if (this != &B) {
         bool same_dims = true;
@@ -155,7 +155,7 @@ template <typename T, std::size_t N> grid<T, N> & grid<T, N>::operator=(const gr
     return *this;
 }
 
-template <typename T, std::size_t N> grid<T, N> & grid<T, N>::operator=(grid && B) noexcept {
+template <typename T, std::size_t N> ndarray<T, N> & ndarray<T, N>::operator=(ndarray && B) noexcept {
     if (this != &B) {
         delete[] data;
 
@@ -171,7 +171,7 @@ template <typename T, std::size_t N> grid<T, N> & grid<T, N>::operator=(grid && 
     return *this;
 }
 
-template <typename T, std::size_t N> grid<T, N>::grid(const grid & grid_b) {
+template <typename T, std::size_t N> ndarray<T, N>::ndarray(const ndarray & grid_b) {
     for (std::size_t i = 0; i < N; ++i) {
         dims[i]    = grid_b.dims[i];
         strides[i] = grid_b.strides[i];
@@ -182,7 +182,7 @@ template <typename T, std::size_t N> grid<T, N>::grid(const grid & grid_b) {
     }
 }
 
-template <typename T, std::size_t N> grid<T, N>::grid(grid && grid_b) noexcept {
+template <typename T, std::size_t N> ndarray<T, N>::ndarray(ndarray && grid_b) noexcept {
     for (std::size_t i = 0; i < N; ++i) {
         dims[i]    = grid_b.dims[i];
         strides[i] = grid_b.strides[i];
