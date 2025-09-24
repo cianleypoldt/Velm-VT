6c1df64a57dca79310d5559eb17e51d859bd501b
Cian Leypoldt <cianleypoldt@gmail.com>
2025-09-24 23:21:42 +0200

    base grid class refactor

--- src/velm_dr/NDField.h
+++ src/velm_dr/NDField.h
@@ -1,77 +1,194 @@
-#include <array>
 #include <cstddef>
-#include <initializer_list>
-#include <stdexcept>
-#include <vector>
+#include <cstdlib>
 
 namespace vlem_DR {
 
-template <typename T, std::size_t N> class NDField {
-    std::vector<T>             data_;
-    std::array<std::size_t, N> dims_;
-    std::array<std::size_t, N> strides_;
+/*
+ * vulnerabilities:
+ *  1. Abort is the only error handling in place currently
+ *  2. If new[] fails in assignment operators, momory could be leaked
+ *
+ */
 
-  public:
-    explicit NDField(const std::array<std::size_t, N> & dims) : dims_(dims) {
-        std::size_t total = 1;
-        for (std::size_t i = N; i-- > 0;) {  // compute strides from last to first
-            strides_[i] = total;
-            total *= dims_[i];
-        }
-        data_.resize(total);
-    }
+template <typename T, std::size_t N> struct grid {
+    T *         data = nullptr;
+    std::size_t dims[N];
+    std::size_t strides[N];
 
-    // Bounds-checked access
-    template <typename... Idx> T & at(Idx... idx) {
-        static_assert(sizeof...(idx) == N, "Wrong number of indices");
-        std::size_t offset = compute_offset(idx...);
-        return data_.at(offset);
-    }
+    template <typename... Idx> grid(Idx... idx);
+    ~grid();
 
-    template <typename... Idx> const T & at(Idx... idx) const {
-        static_assert(sizeof...(idx) == N, "Wrong number of indices");
-        std::size_t offset = compute_offset(idx...);
-        return data_.at(offset);
-    }
+    template <typename... Idx> [[nodiscard]] T &       at(Idx... idx);
+    template <typename... Idx> [[nodiscard]] const T & at(Idx... idx) const;
 
-    // Fast access without bounds checking
-    template <typename... Idx> T & operator()(Idx... idx) {
-        static_assert(sizeof...(idx) == N, "Wrong number of indices");
-        return data_[compute_offset(idx...)];
-    }
+    template <typename... Idx> [[nodiscard]] std::size_t offset_of_index(const Idx &... idx) const;
+    [[nodiscard]] std::size_t                            total_elements() const;
 
-    template <typename... Idx> const T & operator()(Idx... idx) const {
-        static_assert(sizeof...(idx) == N, "Wrong number of indices");
-        return data_[compute_offset(idx...)];
-    }
+    void                    fill(T value);
+    [[nodiscard]] T *       begin();
+    [[nodiscard]] const T * begin() const;
+    [[nodiscard]] T *       end();
+    [[nodiscard]] const T * end() const;
 
-    // Linear access operator
-    T & operator[](std::size_t index) { return data_[index]; }
-
-    const T & operator[](std::size_t index) const { return data_[index]; }
-
-    // Get dimension sizes
-    std::size_t size(std::size_t dim) const {
-        if (dim >= N) {
-            throw std::out_of_range("Dimension out of range");
-        }
-        return dims_[dim];
-    }
-
-    std::array<std::size_t, N> dimensions() const { return dims_; }
-
-  private:
-    template <typename... Idx> std::size_t compute_offset(Idx... idx) const {
-        std::size_t indices[N] = { static_cast<std::size_t>(idx)... };
-        std::size_t offset     = 0;
-        for (std::size_t i = 0; i < N; ++i) {
-            if (indices[i] >= dims_[i]) {
-                throw std::out_of_range("Index out of bounds");
-            }
-            offset += indices[i] * strides_[i];
-        }
-        return offset;
-    }
+    template <typename... Idx> [[nodiscard]] T &       operator()(Idx... idx);
+    template <typename... Idx> [[nodiscard]] const T & operator()(Idx... idx) const;
+    [[nodiscard]] grid &                               operator=(const grid & B);
+    [[nodiscard]] grid &                               operator=(grid && B) noexcept;
+    grid(const grid & B);
+    grid(grid && B) noexcept;
 };
+
+template <typename T, std::size_t N> template <typename... Idx> grid<T, N>::grid(Idx... idx) {
+    static_assert(sizeof...(Idx) == N, "Number of indices must match grid dimension");
+    std::size_t indices[N] = { static_cast<std::size_t>(idx)... };
+    for (std::size_t i = 0; i < N; ++i) {
+        dims[i] = indices[i];
+    }
+
+    strides[N - 1] = 1;
+    for (std::size_t i = N - 1; i > 0; --i) {
+        strides[i - 1] = strides[i] * dims[i];
+    }
+
+    // allocate memory
+    data = new T[total_elements()]{};
+}
+
+template <typename T, std::size_t N> grid<T, N>::~grid() {
+    delete[] data;
+}
+
+template <typename T, std::size_t N> template <typename... Idx> [[nodiscard]] T & grid<T, N>::at(Idx... idx) {
+    std::size_t indices[N] = { static_cast<std::size_t>(idx)... };
+    for (std::size_t i = 0; i < N; ++i) {
+        if (indices[i] >= dims[i]) {
+            abort();
+        }
+    }
+    return data[offset_of_index(idx...)];
+}
+
+template <typename T, std::size_t N> template <typename... Idx> const T & grid<T, N>::at(Idx... idx) const {
+    std::size_t indices[N] = { static_cast<std::size_t>(idx)... };
+    for (std::size_t i = 0; i < N; ++i) {
+        if (indices[i] >= dims[i]) {
+            abort();
+        }
+    }
+    return data[offset_of_index(idx...)];
+}
+
+template <typename T, std::size_t N> template <typename... Idx>
+std::size_t grid<T, N>::offset_of_index(const Idx &... idx) const {
+    static_assert(sizeof...(Idx) == N, "Number of indices must match grid dimension");
+    std::size_t indices[N] = { static_cast<std::size_t>(idx)... };
+    std::size_t offset     = 0;
+    for (std::size_t i = 0; i < N; ++i) {
+        offset += indices[i] * strides[i];
+    }
+    return offset;
+}
+
+template <typename T, std::size_t N> std::size_t grid<T, N>::total_elements() const {
+    std::size_t element_count = 1;
+    for (std::size_t i = 0; i < N; ++i) {
+        element_count *= dims[i];
+    }
+    return element_count;
+}
+
+template <typename T, std::size_t N> T * grid<T, N>::begin() {
+    return data;
+}
+
+template <typename T, std::size_t N> const T * grid<T, N>::begin() const {
+    return data;
+}
+
+template <typename T, std::size_t N> T * grid<T, N>::end() {
+    return data + total_elements();
+}
+
+template <typename T, std::size_t N> const T * grid<T, N>::end() const {
+    return data + total_elements();
+}
+
+template <typename T, std::size_t N> void grid<T, N>::fill(T value) {
+    for (std::size_t i = 0; i < total_elements(); ++i) {
+        data[i] = value;
+    }
+}
+
+template <typename T, std::size_t N> template <typename... Idx> T & grid<T, N>::operator()(Idx... idx) {
+    return data[offset_of_index(idx...)];
+}
+
+template <typename T, std::size_t N> template <typename... Idx> const T & grid<T, N>::operator()(Idx... idx) const {
+    return data[offset_of_index(idx...)];
+}
+
+template <typename T, std::size_t N> grid<T, N> & grid<T, N>::operator=(const grid & B) {
+    // self-assignment check
+    if (this != &B) {
+        bool same_dims = true;
+        for (std::size_t i = 0; i < N && same_dims; ++i) {
+            if (dims[i] != B.dims[i]) {
+                same_dims = false;
+            }
+        }
+
+        // if dimensions are different, deallocate and reallocate
+        if (!same_dims) {
+            delete[] data;
+            for (std::size_t i = 0; i < N; ++i) {
+                dims[i]    = B.dims[i];
+                strides[i] = B.strides[i];
+            }
+            data = new T[total_elements()];
+        }
+
+        // copy the data
+        for (std::size_t i = 0; i < total_elements(); ++i) {
+            data[i] = B.data[i];
+        }
+    }
+    return *this;
+}
+
+template <typename T, std::size_t N> grid<T, N> & grid<T, N>::operator=(grid && B) noexcept {
+    if (this != &B) {
+        delete[] data;
+
+        for (std::size_t i = 0; i < N; ++i) {
+            dims[i]    = B.dims[i];
+            strides[i] = B.strides[i];
+        }
+
+        // transfer ownership of data
+        data   = B.data;
+        B.data = nullptr;
+    }
+    return *this;
+}
+
+template <typename T, std::size_t N> grid<T, N>::grid(const grid & grid_b) {
+    for (std::size_t i = 0; i < N; ++i) {
+        dims[i]    = grid_b.dims[i];
+        strides[i] = grid_b.strides[i];
+    }
+    data = new T[total_elements()];
+    for (std::size_t i = 0; i < total_elements(); ++i) {
+        data[i] = grid_b.data[i];
+    }
+}
+
+template <typename T, std::size_t N> grid<T, N>::grid(grid && grid_b) noexcept {
+    for (std::size_t i = 0; i < N; ++i) {
+        dims[i]    = grid_b.dims[i];
+        strides[i] = grid_b.strides[i];
+    }
+    data        = grid_b.data;
+    grid_b.data = nullptr;
+}
 
 };  // namespace vlem_DR
